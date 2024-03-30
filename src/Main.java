5ffd5176340c0158644387606129a1107943ec14
ecemtellal <91099163+ecemtellal@users.noreply.github.com>
2024-03-30 22:47:34 +0300

    done commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int mat,fizik,kimya,turkce,tarih,muzik;
+       int totalPoint=0,lessonCount=6 ,passPoint=60;
         Scanner input=new Scanner(System.in);
         System.out.println("Matematik notunuzu giriniz:");
-        mat=input.nextInt();
+        totalPoint+=input.nextInt();
         System.out.println("Fizik notunuzu giriniz:");
-        fizik=input.nextInt();
+        totalPoint+=input.nextInt();
         System.out.println("Kimya notunuzu giriniz:");
-        kimya=input.nextInt();
+        totalPoint+=input.nextInt();
         System.out.println("Türkçe notunuzu giriniz:");
-        turkce=input.nextInt();
+        totalPoint+=input.nextInt();
         System.out.println("Tarih notunuzu giriniz:");
-        tarih=input.nextInt();
+        totalPoint+=input.nextInt();
         System.out.println("Müzik notunuzu giriniz:");
-        muzik=input.nextInt();
-        int ort=(mat+fizik+tarih+kimya+turkce+muzik)/6;
-        System.out.println("Ders Ortalamanız:" +ort );
-        String str= ort > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
-        System.out.println(str);
+        totalPoint+=input.nextInt();
+        int average=(totalPoint)/lessonCount;
+        System.out.println("Ders Ortalamanız:" + average);
+        String userInfo= average > passPoint ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(userInfo);
 
 
     }}
