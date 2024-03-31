1adc5042448623e42fc59cccb7d65ca3a393224f
ecemtellal <91099163+ecemtellal@users.noreply.github.com>
2024-03-31 04:26:44 +0300

    done commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
        int totalPoint=0,lessonCount=6 ,passPoint=60, average;
        String userInfo;
        //tanımlamalar en üstte ve ing olursa eğer kod okunması için daha iyi
        //total point:Toplam Puan,lesson count:Ders Sayısı,pass point :geçiş notu
-        //1 defa yazılıyor üstteki
+       //user info: Kullanıcı Bilgisi,average :ortalama demek
+
         Scanner input=new Scanner(System.in);
+        // Scanner input=new Scanner(System.in); sadece 1 defa yazılıyor
+
         System.out.println("Matematik notunuzu giriniz:");
-        /* sout altına sadece 1 satır totalPoint+=input.nextInt();
-          bunun gibi bir girdi yapılabilir fazla olunca çıktıda
+        /* sout altına totalPoint+=input.nextInt();
+          bunun gibi bir sadece 1 satır girdi yapılabilir, fazla olunca çıktıda
          girdi sayısı kadar sayı girmeni ister */
+
         totalPoint+=input.nextInt();
-        //+= her seferinde puan ekle demek
+        //+= her seferinde üzerine puan ekliyor
         System.out.println("Fizik notunuzu giriniz:");
         totalPoint+=input.nextInt();
         System.out.println("Kimya notunuzu giriniz:");
@@ -23,11 +28,14 @@ public class Main {
         totalPoint+=input.nextInt();
         System.out.println("Müzik notunuzu giriniz:");
         totalPoint+=input.nextInt();
+
         average=(totalPoint)/lessonCount;
         System.out.println("Ders Ortalamanız:" + average);
+
         userInfo= average > passPoint ? "Sınıfı Geçti" : "Sınıfta Kaldı";
         //ternary operatör kullanımı üstteki gibi
-        //user info: Kullanıcı Bilgisi,average :ortalama
+
+
         System.out.println(userInfo);
         //kodun okunması ve kullanışlı olması için alt kısımda rakam olmamasına dikkat et!
     }}
